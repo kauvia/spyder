2ca50b221befb7966d2ef2dc88d0822f1f136973
kauvia <mkhw1989@gmail.com>
2019-02-02 17:57:53 +0800

    added in testing info

--- server/app/controllers/test_controller.rb
+++ server/app/controllers/test_controller.rb
@@ -1,10 +1,24 @@
 class TestController < ApplicationController
     before_action :authenticate_user!
 
+  ######################################################
+  #
+  # FOR ALL CONTROLLERS THAT ACCESS SENSITIVE INFO SUCH AS 
+  # SPECIFIC USER REQUESTS (EG: LISTING ALL FOOD/STAT/EXERCISE)
+  # MAKE SURE "before_action :authenticate_user!" IS IN THAT CONTROLLER
+  # (basically all controllers except login/logout/signup)
+  #
+  # AFTER THAT, THE CURRENT USER IS ACCESSIBLE WITH "current_user"
+  # SO IF U WANT TO RETRIEVE ALL FOOD/STAT/EXERCISE OR ANY OTHER KIND OF
+  # INFO RELATED TO THAT USER, JUST USE current_user.food OR ANY OTHER
+  # REGULAR ACTIVE-RECORD SEARCH METHODS PERTAINING TO THAT USER 
+  #       SEE EXAMPLE BELOW( CAN BE TESTED VIA CLIENT AT LOGIN PAGE<CHECK CONSOLE>)
+  #########################################################
 
     def index
-      p session[:user_id]
-      render :json => {"hello"=>"world"}
+      p current_user.food
+      p current_user.stat
+      render :json => {"hello"=>"world","exercise"=>current_user.exercise, "food"=>current_user.food}
     end
   
   end
