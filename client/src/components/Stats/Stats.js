f51be87d495a885da8d460063d561510cdceddc5
andrealmj <andrealmj@live.com>
2019-02-07 15:02:42 +0800

    input formula to calculate calorie allowance

--- client/src/components/Stats/Stats.js
+++ client/src/components/Stats/Stats.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { api } from '../functions';
+import { api } from '../functions'
 import AllowanceContainer from '../Allowance-Container/AllowanceContainer'
 
 class Stats extends Component {
     constructor(props){
         super(props)
         this.state = {
-            statHistory : {}
+            statHistory: {}
         }
 }
     componentDidMount() {
         api("GET", "stats").then(val => {this.setState({ statHistory : val.data.stat }); console.log(this.state)})
-        // console.log(this.props.statistory)
     }
 
     render(){
@@ -23,7 +22,7 @@ class Stats extends Component {
 
                     Height: {this.state.statHistory[0].height}cm<br/>
                     Weight: {this.state.statHistory[0].weight}kg<br />
-                    Target Weight: {this.state.statHistory[0].weight}kg<br />
+                    Target Weight: {this.state.statHistory[0].target_weight}kg<br />
                     Activity Level: {this.state.statHistory[0].activity_level}<br />
 
                     <button>Edit Stats</button>
